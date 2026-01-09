0fb40ddc455dbdff1e2cdd2a10ff5721158164ef
Brahim <Jabash Senior Shield@DESKTOP-GFARDVA>
2026-01-09 11:54:14 +0100

    UserController aangemaakt en werkend voor inlogsysteem

---
+++ JABASH_SeniorShield/Controller/UserController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using JABASH_SeniorShield.Model;
+
+namespace JABASH_SeniorShield.Controller
+{
+    internal class UserController
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["SeniorShieldDbConn"].ConnectionString;
+
+        public UserModel Login(string email, string wachtwoord)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT UserId, Voornaam, Achternaam, Email, Wachtwoord FROM tblUsers WHERE Email = @Email AND Wachtwoord = @Wachtwoord";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                // Tegen SQL Injecties voor Cyber Security adviezen
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Wachtwoord", wachtwoord);
+
+                try
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        return new UserModel
+                        {
+                            UserId = Convert.ToInt32(reader["UserId"]),
+                            Voornaam = reader["Voornaam"].ToString(),
+                            Achternaam = reader["Achternaam"].ToString(),
+                            Email = reader["Email"].ToString()
+                        };
+                    }
+                }
+                catch
+                {
+                    throw new Exception("Er is een probleem met de database connectie");
+                }
+
+                return null;
+            }
+        }
+    }
+
+}
