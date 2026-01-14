c9b27d86202816640401665639bb0867b0f78419
Brahim <Jabash Senior Shield@DESKTOP-GFARDVA>
2026-01-14 10:58:57 +0100

    Mailbox Layout

--- JABASH_SeniorShield/View/Mailbox/frmMailbox.Designer.cs
+++ JABASH_SeniorShield/View/Mailbox/frmMailbox.Designer.cs
@@ -31,31 +31,44 @@
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.PaleGreen;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 26);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 114);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 200);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 297);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // flowLayoutPanel2
             // 
             this.flowLayoutPanel2.BackColor = System.Drawing.Color.PeachPuff;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 247);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 545);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(776, 200);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(776, 56);
             this.flowLayoutPanel2.TabIndex = 1;
             // 
             // flowLayoutPanel3
             // 
             this.flowLayoutPanel3.BackColor = System.Drawing.Color.LightCoral;
-            this.flowLayoutPanel3.Location = new System.Drawing.Point(12, 467);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(12, 622);
             this.flowLayoutPanel3.Name = "flowLayoutPanel3";
-            this.flowLayoutPanel3.Size = new System.Drawing.Size(776, 200);
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(776, 45);
             this.flowLayoutPanel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkGreen;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(11, 9, 0, 0);
+            this.label1.Size = new System.Drawing.Size(776, 57);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Veilige Post";
             // 
             // frmMailbox
             // 
@@ -63,6 +76,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 744);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.flowLayoutPanel3);
             this.Controls.Add(this.flowLayoutPanel2);
             this.Controls.Add(this.flowLayoutPanel1);
@@ -78,5 +92,6 @@
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Label label1;
     }
 }
